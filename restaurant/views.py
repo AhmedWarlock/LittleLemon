af1d424fcfa5b61341023d9c6bade1edf685b2ac
Ahmed Awad <ahmedrhama99@gmail.com>
2023-12-14 13:43:30 +0200

    added serializers and connected to MySQL database

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView (generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
